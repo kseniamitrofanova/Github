ae85ceee78d58beabef0cb4b4511f4eea6a1d08d
KMitrofanova <vip.ksundra@mail.ru>
2023-11-02 21:43:06 +0300

    added JUnitFound.java

---
+++ src/test/java/github/JUnitFound.java
@@ -0,0 +1,42 @@
+package github;
+
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+
+
+
+public class JUnitFound {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void FindJUnit5(){
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("[placeholder='Find a page…']").setValue("SoftAssertions").pressEnter();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertion"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").scrollIntoView(true);
+        $("#user-content-3-using-junit5-extend-test-class").sibling(0).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
